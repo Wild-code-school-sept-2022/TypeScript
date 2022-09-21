b524b40d87b374dd393f57c6f532547ab56c8bb7
dldvlpr <dormoy.dev@gmail.com>
2022-09-21 11:58:30 +0200

    -n

--- challenge.ts
+++ challenge.ts
@@ -12,9 +12,9 @@ interface user {
 }
 
 const wilders: [user?] = [];
-const user1 = { name: "Pierre", age: 23 };
-const user2 = { name: "Paul", birthday: "10/02/1990" };
-const user3 = { name: "Jacques", age: 25 };
+const user1: user = { name: "Pierre", age: 23 };
+const user2: user = { name: "Paul", birthday: "10/02/1990" };
+const user3: user = { name: "Jacques", age: 25 };
 wilders.push(user1);
 wilders.push(user2);
 wilders.push(user3);
